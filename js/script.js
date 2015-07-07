9c417111fbb235375ab028e338a09c4921d13d01
Hieren Feb <feb@geekpolis.com>
2015-07-07 23:57:32 +0700

    update

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@
     $(".filter .form-select").chosen({
       width: '100%',
     });
+    $(".show-code").click(function(){
+      $(this).next(".component--source").toggleClass("element-invisible");
+    });
+    $(".hide-code").click(function(){
+      $(this).parent(".component--source").addClass("element-invisible");
+    });
   });
 } )( jQuery );
