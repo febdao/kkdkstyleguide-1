0aba7fc752e6b6e677c1916fab44ca02b552d047
Thanh Do <huuthanhcntt3a2@gmail.com>
2015-07-07 17:18:59 +0700

    update component

--- js/script.js
+++ js/script.js
@@ -3,5 +3,12 @@
     $(".filter .form-select").chosen({
       width: '100%',
     });
+
+    $('.pane-menu-block-front-page-menu').attr('class','reg-inner-regions');
+
+    $( ".accordion" ).accordion({
+      heightStyle: "content",
+      collapsible: true
+    });
   });
 } )( jQuery );
